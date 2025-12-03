26153ddba0a29fc9cb60f09bcbba7db86aa308f1
Eric Magno <ericvincentmagno@gmail.com>
2025-12-04 07:16:39 +0800

    first commit

---
+++ ticketflash-booking-service/src/main/java/com/ericmagno/ticketflash_booking_service/TicketflashBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ericmagno.ticketflash_booking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketflashBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketflashBookingServiceApplication.class, args);
+	}
+
+}
---
+++ ticketflash-booking-service/src/test/java/com/ericmagno/ticketflash_booking_service/TicketflashBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericmagno.ticketflash_booking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketflashBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
