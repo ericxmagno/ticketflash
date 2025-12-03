3794cf93b0e8beae41cc0d91014d7b710cfce3c8
Eric Magno <ericvincentmagno@gmail.com>
2025-12-04 07:13:50 +0800

    first commit

---
+++ ticketflash-notification-service/src/test/java/com/ericmagno/ticketflash_notification_service/TicketflashNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericmagno.ticketflash_notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketflashNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ticketflash-order-service/src/main/java/com/ericmagno/ticketflash_order_service/TicketflashOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ericmagno.ticketflash_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketflashOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketflashOrderServiceApplication.class, args);
+	}
+
+}
